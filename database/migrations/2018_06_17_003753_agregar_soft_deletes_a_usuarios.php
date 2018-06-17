85fca5809b7424ab3ff20c68484b5272f91e2e34
Matías Olivera <matiasolivera@outlook.com>
2018-06-16 21:47:38 -0300

    Agregar soft deletes a la tabla usuarios

---
+++ database/migrations/2018_06_17_003753_agregar_soft_deletes_a_usuarios.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AgregarSoftDeletesAUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')
+                ->unique()
+                ->change();
+
+            // Deleted_at
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('username');
+            $table->dropSoftDeletes();
+        });
+    }
+}
