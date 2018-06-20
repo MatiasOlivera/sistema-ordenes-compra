7ebe06c0fcf36a65c66eb5957193eb8cd773324b
CIrel Romeo <cirelhromeo@gmail.com>
2018-06-20 01:01:06 -0300

    CREA: Controlador, Modelo y Blade de PersonaFisica

---
+++ app/Http/Controllers/PersonaFisicaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PersonaFisica;
+use Illuminate\Http\Request;
+
+class PersonaFisicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PersonaFisica  $personaFisica
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PersonaFisica $personaFisica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PersonaFisica  $personaFisica
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PersonaFisica $personaFisica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PersonaFisica  $personaFisica
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PersonaFisica $personaFisica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PersonaFisica  $personaFisica
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PersonaFisica $personaFisica)
+    {
+        //
+    }
+}
---
+++ resources/views/fisicas.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('container')
+  <h1>Fisicas</h1>
+
+@endsection
