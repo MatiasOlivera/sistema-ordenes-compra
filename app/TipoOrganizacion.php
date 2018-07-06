72b2a49f781957c749add61addc6dce8151a73e7
Matías Olivera <matiasolivera@outlook.com>
2018-07-06 17:29:10 -0300

    Agregar el modelo TipoOrganizacion

---
+++ app/TipoOrganizacion.php
@@ -0,0 +1,36 @@
+<<<<<<< current
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TipoOrganizacion extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tipos_organizacion';
+
+    /**
+    * The attributes that should be mutated to dates.
+    *
+    * @var array
+    */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'descripcion'
+    ];
+
+}
