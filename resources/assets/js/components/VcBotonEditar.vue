70292ca3949ecd93c9783eb96622b37d6edfc4c3
Matías Olivera <matiasolivera@outlook.com>
2018-07-06 18:40:59 -0300

    Agregar los componentes VcBotonEditar y VcBotonEliminar

---
+++ resources/assets/js/components/VcBotonEditar.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+
+    <button type="button" name="editar" class="btn btn-sm btn-outline-primary">
+        <edit-icon class="icono"></edit-icon> Editar
+    </button>
+
+</template>
+
+<script>
+import { EditIcon } from 'vue-feather-icons';
+
+export default {
+    components: { EditIcon }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/VcBotonEliminar.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+
+    <button type="button" name="eliminar" class="btn btn-sm btn-outline-danger">
+        <trash-icon class="icono"></trash-icon> Eliminar
+    </button>
+
+</template>
+
+<script>
+import { TrashIcon } from 'vue-feather-icons';
+
+export default {
+    components: { TrashIcon }
+}
+</script>
+
+<style lang="css">
+</style>
