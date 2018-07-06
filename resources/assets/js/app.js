ba2e6da1e27ff383dbe0abdffcb1eb80a1241d7a
Matías Olivera <matiasolivera@outlook.com>
2018-07-06 18:07:00 -0300

    Agregar configuración de vue-tables-2

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,3 +8,49 @@
 require('./libraries');
 
 window.Vue = require('vue');
+
+/**
+ * Vue-tables-2
+ */
+
+import { ServerTable, Event } from 'vue-tables-2';
+
+const OpcionesVueTables = {
+    pagination: {
+        chunk: 5
+    },
+    perPage: 10,
+    perPageValues: [10, 15, 20],
+    responseAdapter(response) {
+        return {
+            data:  response.data.data,
+            count: response.data.total
+        }
+    },
+    skin: 'table table-bordered table-hover',
+    sortIcon: {
+        base: '',
+        up:   '',
+        down: '',
+        is:   ''
+    },
+    saveState: true,
+    storage: 'local',
+    texts: {
+        count:             'Mostrando del {from} al {to} de {count} registros|{count} registros|Un registro',
+        first:             'Primer',
+        last:              'Último',
+        filter:            'Filtro:',
+        filterPlaceholder: '¿Qué estás buscando?',
+        limit:             'Registros:',
+        page:              'Página:',
+        noResults:         'No existen registros que coincidan con la búsqueda o no hay ningún registro guardado',
+        filterBy:          'Filtrar por {column}',
+        loading:           'Cargando...',
+        defaultOption:     'Seleccionar {column}',
+        columns:           'Columnas'
+    }
+}
+
+Vue.use(ServerTable, OpcionesVueTables, false, 'bootstrap4', 'footerPagination');
+
