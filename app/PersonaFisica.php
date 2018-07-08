b39d4caa108d9fed6b8caaf7540b4f88be8d06fe
CIrel Romeo <cirelhromeo@gmail.com>
2018-07-08 19:58:07 -0300

    Agrega configuracion a  PersonaFisica
    espesifica la tabla de la BD, campos de asignables de forma masiva, SoftDelete, y relacion con la clase persona

--- app/PersonaFisica.php
+++ app/PersonaFisica.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PersonaFisica extends Model
 {
-    //
+    use SoftDeletes;
+    
+    protected $table = 'personas_fisicas';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = ['nombres', 'apellidos', 'documento', 'fecha_nacimiento', 'sexo'];
+
+    /**
+    * The attributes that should be mutated to dates.
+    *
+    * @var array
+    */
+
+    protected $dates = ['created_at', 'updated_at','deleted_at'];
+
+    /**
+     * obtener persona relacionada con esta personaFisica
+     */
+
+    public function persona()
+    {
+        return $this->belongsTo('App\Persona', 'id', 'id');
+    }
 }
