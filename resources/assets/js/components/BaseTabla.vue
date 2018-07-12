553535b4f9eab7a06770806a5326778d7238929e
Matías Olivera <matiasolivera@outlook.com>
2018-07-12 16:01:09 -0300

    Ver los registros eliminados en BaseTabla

--- resources/assets/js/components/BaseTabla.vue
+++ resources/assets/js/components/BaseTabla.vue
@@ -3,10 +3,23 @@
     <v-server-table
         :ref="nombre"
         :name="nombre"
-        :url="url"
         :columns="todasColumnas"
-        :options="opciones"
+        :options="todasOpciones"
     >
+        <template slot="afterFilter">
+            <div class="custom-control custom-checkbox ml-3">
+                <input
+                    v-model="soloEliminados"
+                    type="checkbox"
+                    class="custom-control-input"
+                    id="eliminados"
+                >
+                <label class="custom-control-label" for="eliminados">
+                    Solo eliminados
+                </label>
+            </div>
+        </template>
+    
         <template slot="perfil" slot-scope="{ row: {id} }">
             <button @click="verPerfil(id)" type="button" name="perfil"
             class="btn btn-sm btn-outline-info">
@@ -45,43 +58,87 @@ export default {
             type: String,
             required: true
         },
+        
         url: {
             type: String,
             required: true
         },
+        
         columnas: {
             type: Array,
             required: true
         },
+        
         opciones: {
             type: Object,
             required: true
         },
+        
         obtenerTabla: {
             type: Boolean,
             default: false
         },
+        
         campoNombre: {
             type: String,
             required: true
         }
     },
+    data() {
+        return {
+            soloEliminados: false
+        }
+    },
     computed: {
         todasColumnas() {
             const especificas = this.columnas;
-            const porDefecto  = ['perfil', 'editar', 'eliminar'];
+            const porDefecto = ['perfil', 'editar', 'baja'];
+
+            /**
+             * Ocultar las columnas editar y eliminar
+             * cuando se ven los registros eliminados
+             */
+            if (this.soloEliminados) {
+                porDefecto.splice(1, 2);
+                porDefecto.push('alta');
+            }
+            
             return especificas.concat(porDefecto);
+        },
+        
+        todasOpciones() {
+            let opcionesPorDefecto = {
+                requestFunction: (data) => {
+                    let binario = this.soloEliminados ? 1 : 0;
+                    let eliminados = { soloEliminados: binario };
+                    
+                    let parametros = { ...data, ...eliminados };
+                    
+                    return axios.get(this.url, {
+                        params: parametros
+                    }).catch(function (error) {
+                        this.dispatch('error', error);
+                    }.bind(this));
+                }
+            };
+            
+            return {...opcionesPorDefecto, ...this.opciones};
         }
     },
     watch: {
         obtenerTabla(estado) {
             if (estado) this.obtenerModelos();
+        },
+        
+        soloEliminados() {
+            this.obtenerModelos();
         }
     },
     methods: {
         verPerfil(id) {
             this.$emit('ver-perfil', id);
         },
+        
         editar(id) {
             this.$emit('editar', id);
         },
