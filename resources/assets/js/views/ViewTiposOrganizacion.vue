588429a8aab43204c6aa5c680db7c92403f5b923
Matías Olivera <matiasolivera@outlook.com>
2018-07-14 13:35:06 -0300

    Agregar la vista ViewTiposOrganizacion

---
+++ resources/assets/js/views/ViewTiposOrganizacion.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+    
+    <base-crud
+        :titulo="titulo"
+        :botonNuevo="botonNuevo"
+    >
+    
+        <template slot="perfil" slot-scope="{ mostrarForm, ocultarPerfil }">
+            <vc-perfil-tipo-org
+                @mostrar-form="mostrarForm(false)"
+                @cerrar="ocultarPerfil"
+            >
+            </vc-perfil-tipo-org>
+        </template>
+        
+        <template slot="formulario" slot-scope="{ ocultarForm }">
+            <vc-form-tipo-org @cerrar="ocultarForm">
+            </vc-form-tipo-org>
+        </template>
+        
+        <template slot="tabla" slot-scope="{ mostrarPerfil, mostrarForm }">
+            <vc-tabla-tipo-org
+                @mostrar-perfil="mostrarPerfil"
+                @mostrar-form="mostrarForm(true)"
+            >    
+            </vc-tabla-tipo-org>
+        </template>
+    
+    </base-crud>
+    
+</template>
+
+<script>
+import BaseCrud        from '../views/BaseCrud.vue';
+import VcPerfilTipoOrg from '../components/tipo_organizacion/VcPerfilTipoOrg.vue';
+import VcFormTipoOrg   from '../components/tipo_organizacion/VcFormTipoOrg.vue';
+import VcTablaTipoOrg  from '../components/tipo_organizacion/VcTablaTipoOrg.vue';
+
+export default {
+    components: {
+        BaseCrud,
+        VcPerfilTipoOrg,
+        VcFormTipoOrg,
+        VcTablaTipoOrg
+    },
+    data() {
+        return {
+            titulo: 'Tipos de organización',
+            botonNuevo: 'Nuevo'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
