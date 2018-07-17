0fb8ed770098516f3f01eeaab2413ce4c7626054
Matías Olivera <matiasolivera@outlook.com>
2018-07-17 20:08:27 -0300

    Agregar el componente VcIconoEstado

---
+++ resources/assets/js/components/VcIconoEstado.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+    
+    <div>
+        <check-icon
+            v-if="estado"
+            class="icono"
+        >
+        </check-icon>
+        
+        <x-icon
+            v-else
+            class="icono"
+        >
+        </x-icon>
+    </div>
+    
+</template>
+
+<script>
+import { CheckIcon, XIcon } from 'vue-feather-icons';
+
+export default {
+    name: 'vc-icono-estado',
+    components: {
+        CheckIcon,
+        XIcon
+    },
+    props: {
+        estado: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
