eca9aa7ee9e2b5b2b86d28f0717bbb341f015ba4
Matías Olivera <matiasolivera@outlook.com>
2018-07-21 22:18:03 -0300

    Agregar el mixin multiselect_config
    Contiene la configuración por defecto de los select.
    
    Por qué no un componente base?
    - Evitar tener que definir la misma interfaz del componente Multiselect en el componente base (repetir las props y los eventos)
    - Existen props que solo se usan en ciertos casos, por lo que habría que recurrir al método render (o similar) para generar el componente base. Ej: prop label / customLabel

---
+++ resources/assets/js/mixins/multiselect_config_mixin.js
@@ -0,0 +1,26 @@
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+
+export default {
+    components: {
+        Multiselect
+    },
+    data() {
+        return {
+            searchable: true,
+            placeholder: 'Selecciona una opción',
+            internalSearch: true,
+            selectLabel: 'Presiona enter para seleccionar',
+            selectGroupLabel: 'Presiona enter para seleccionar el grupo',
+            selectedLabel: 'Seleccionado',
+            deselectLabel: 'Presiona enter para deseleccionar',
+            deselectGroupLabel: 'Presiona enter para deseleccionar el grupo',
+            openDirection: 'bottom'
+        }
+    },
+    methods: {
+        limitText(count) {
+            return `y ${count} más`;
+        }
+    }
+}
