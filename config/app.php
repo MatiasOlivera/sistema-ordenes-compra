3c19b3e7017fb0add14ce5ef20bf2182d4c0fcf3
Matías Olivera <matiasolivera@outlook.com>
2018-07-22 21:58:23 -0300

    Agregar la traducción al español

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -159,7 +159,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        /**
+         * Traducción al español
+         */
+        Laraveles\Spanish\SpanishServiceProvider::class,
     ],
 
     /*
