caa0ad94a1aca0052e07c3a88a59f9e4134d88f2
Matías Olivera <matiasolivera@outlook.com>
2018-07-30 23:22:02 -0300

    Crear el componente VcDetalleEmpresa
    El componente VcPerfilEmpresa se encarga de mostrar la información de la empresa en diferentes tarjetas.

---
+++ resources/assets/js/components/empresa/VcDetalleEmpresa.vue
@@ -0,0 +1,202 @@
+<template lang="html">
+    
+    <base-perfil
+        :titulo="titulo"
+        :nombreInstancia="empresa.nombre_fantasia"
+        :eliminado="eliminado"
+        @editar="editar"
+        @dar-de-baja="darDeBaja"
+        @dar-de-alta="darDeAlta"
+        @cerrar="cerrar"
+    >
+    
+        <template slot="acciones">
+        
+            <button
+                v-if="esProveedor"
+                @click="bajaProveedor"
+                type="button"
+                name="bajaProveedor"
+                class="btn btn-sm btn-outline-warning"
+            >
+                <truck-icon class="icono"></truck-icon> Baja proveedor
+            </button>
+            
+            <button
+                v-else
+                @click="altaProveedor"
+                type="button"
+                name="altaProveedor"
+                class="btn btn-sm btn-outline-primary"
+            >
+                <truck-icon class="icono"></truck-icon> Alta proveedor
+            </button>
+        
+        </template>
+    
+        <template>
+            
+            <dl>
+                <dt>Nombre de fantasía:</dt>
+                <dd>{{ empresa.nombre_fantasia }}</dd>
+                
+                <dt>Mayorista:</dt>
+                <dd>
+                    <vc-icono-estado :estado="esMayorista">
+                    </vc-icono-estado>
+                </dd>
+                
+                <dt>Proveedor:</dt>
+                <dd>
+                    <vc-icono-estado :estado="esProveedor">
+                    </vc-icono-estado>
+                </dd>
+                
+                <dt>Creado:</dt>
+                <dd>
+                    {{ empresa.created_at | moment('from') }}, 
+                    {{ empresa.created_at | moment('L LT a') }}
+                </dd>
+                
+                <dt>Actualizado:</dt>
+                <dd>
+                    {{ empresa.updated_at | moment('from') }}, 
+                    {{ empresa.updated_at | moment('L LT a') }}
+                </dd>
+                
+                <template v-if="eliminado">
+                    <dt>Eliminado:</dt>
+                    <dd>
+                        {{ empresa.deleted_at | moment('from') }}, 
+                        {{ empresa.deleted_at | moment('L LT a') }}
+                    </dd>
+                </template>
+            </dl>
+            
+        </template>
+    
+    </base-perfil>
+    
+</template>
+
+<script>
+/**
+ * Mixins
+ */
+ import DarBajaInstanciaMixin from '../../mixins/dar_baja_instancia_mixin.js';
+ import DarAltaInstanciaMixin from '../../mixins/dar_alta_instancia_mixin.js';
+ 
+/**
+ * Componentes
+ */
+import { TruckIcon } from 'vue-feather-icons';
+import BasePerfil    from '../../components/BasePerfil.vue';
+import VcIconoEstado from '../../components/VcIconoEstado.vue';
+
+
+export default {
+    name: 'vc-detalle-empresa',
+    mixins: [
+        DarBajaInstanciaMixin,
+        DarAltaInstanciaMixin
+    ],
+    components: {
+        BasePerfil,
+        VcIconoEstado,
+        TruckIcon
+    },
+    props: {
+        empresa: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        titulo() {
+            return `Perfil de ${this.empresa.nombre_fantasia}`;
+        },
+        
+        esMayorista() {
+            return this.empresa.es_mayorista === 1 ? true : false;
+        },
+        
+        esProveedor() {
+            return ! _.isNull(this.empresa.proveedor);
+        },
+                
+        eliminado() {
+            return ! _.isNull(this.empresa.deleted_at);
+        },
+        
+        urlEspecifica() {
+            return `${this.$options.static.url.empresas}/${this.empresa.id}`;
+        }
+    },
+    static: {
+        url: {
+            empresas: '/empresas'
+        }
+    },
+    methods: {        
+        editar() {
+            this.$emit('editar');
+        },
+        
+        altaProveedor() {
+            axios.post(`${this.urlEspecifica}/proveedor`)
+            .then((response) => {
+                this.$emit('alta-proveedor');
+                BusEventos.$emit('VcPerfilEmpresa:altaProveedor');
+                
+                this.exito({
+                    title: 'Éxito',
+                    message: response.data.mensaje
+                });
+            })
+            .catch((error) => {
+                this.error({
+                    title: 'Error',
+                    message: error.response.data.mensaje
+                });
+            })
+        },
+        
+        bajaProveedor() {
+            this.$_darBajaInstanciaMixin_eliminar(
+                `${this.urlEspecifica}/proveedor`,
+                () => {
+                    this.$emit('baja-proveedor');
+                    BusEventos.$emit('VcPerfilEmpresa:bajaProveedor');
+                }
+            );
+        },
+        
+        darDeBaja() {
+            this.$_darBajaInstanciaMixin_eliminar(
+                this.urlEspecifica,
+                () => {
+                    this.$emit('dar-de-baja');
+                    BusEventos.$emit('VcPerfilEmpresa:eliminada')
+                }
+            );
+        },
+        
+        darDeAlta() {
+            this.$_darAltaInstanciaMixin_restaurar(
+                `${this.urlEspecifica}/restore`,
+                () => {
+                    this.$emit('dar-de-alta');
+                    BusEventos.$emit('VcPerfilEmpresa:restaurada')
+                }
+            )
+        },
+        
+        cerrar() {
+            this.$emit('cerrar');
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
--- resources/assets/js/components/empresa/VcPerfilEmpresa.vue
+++ resources/assets/js/components/empresa/VcPerfilEmpresa.vue
@@ -1,144 +1,62 @@
 <template lang="html">
     
-    <base-perfil
-        :titulo="titulo"
-        :nombreInstancia="empresa.nombre_fantasia"
-        :eliminado="eliminado"
-        @editar="editar"
-        @dar-de-baja="darDeBaja"
-        @dar-de-alta="darDeAlta"
-        @cerrar="cerrar"
-    >
-    
-        <template slot="acciones">
+    <div>
+        <!-- Detalle de la empresa -->
+        <vc-detalle-empresa
+            v-show="ui.detalle.visible"
+            :empresa="empresa"
+            @editar="editarEmpresa"
+            @dar-de-baja="obtener"
+            @dar-de-alta="obtener"
+            @alta-proveedor="obtener"
+            @baja-proveedor="obtener"
+            @cerrar="cerrar"
+        >
+        </vc-detalle-empresa>
         
-            <button
-                v-if="esProveedor"
-                @click="bajaProveedor"
-                type="button"
-                name="bajaProveedor"
-                class="btn btn-sm btn-outline-warning"
-            >
-                <truck-icon class="icono"></truck-icon> Baja proveedor
-            </button>
-            
-            <button
-                v-else
-                @click="altaProveedor"
-                type="button"
-                name="altaProveedor"
-                class="btn btn-sm btn-outline-primary"
-            >
-                <truck-icon class="icono"></truck-icon> Alta proveedor
-            </button>
-        
-        </template>
-    
-        <template>
-            
-            <dl>
-                <dt>Nombre de fantasía:</dt>
-                <dd>{{ empresa.nombre_fantasia }}</dd>
-                
-                <dt>Mayorista:</dt>
-                <dd>
-                    <vc-icono-estado :estado="esMayorista">
-                    </vc-icono-estado>
-                </dd>
-                
-                <dt>Proveedor:</dt>
-                <dd>
-                    <vc-icono-estado :estado="esProveedor">
-                    </vc-icono-estado>
-                </dd>
-                
-                <template v-if="conJuridicas">
-                    <dt>Personas jurídicas:</dt>
-                    <dd>
-                        <ul class="list-unstyled">
-                            <li v-for="juridica in empresa.juridicas">
-                                {{ juridica.denominacion }}
-                            </li>
-                        </ul>
-                    </dd>
-                </template>
-                
-                <dt>Creado:</dt>
-                <dd>
-                    {{ empresa.created_at | moment('from') }}, 
-                    {{ empresa.created_at | moment('L LT a') }}
-                </dd>
-                
-                <dt>Actualizado:</dt>
-                <dd>
-                    {{ empresa.updated_at | moment('from') }}, 
-                    {{ empresa.updated_at | moment('L LT a') }}
-                </dd>
-                
-                <template v-if="eliminado">
-                    <dt>Eliminado:</dt>
-                    <dd>
-                        {{ empresa.deleted_at | moment('from') }}, 
-                        {{ empresa.deleted_at | moment('L LT a') }}
-                    </dd>
-                </template>
-            </dl>
-            
-        </template>
-    
-    </base-perfil>
+        <vc-form-empresa
+            v-show="!ui.detalle.visible"
+            @cerrar="verEmpresa"
+        >
+        </vc-form-empresa>
+    </div>
     
 </template>
 
 <script>
-import { TruckIcon }         from 'vue-feather-icons';
-import BasePerfil            from '../../components/BasePerfil.vue';
-import VcIconoEstado         from '../../components/VcIconoEstado.vue';
+/**
+ * Mixins
+ */
 import ObtenerInstanciaMixin from '../../mixins/obtener_instancia_mixin.js';
-import DarBajaInstanciaMixin from '../../mixins/dar_baja_instancia_mixin.js';
-import DarAltaInstanciaMixin from '../../mixins/dar_alta_instancia_mixin.js';
+
+/**
+ * Componentes
+ */
+import VcDetalleEmpresa from './VcDetalleEmpresa.vue';
+import VcFormEmpresa    from './VcFormEmpresa';
 
 export default {
     name: 'vc-perfil-empresa',
+    mixins: [ ObtenerInstanciaMixin ],
     components: {
-        BasePerfil,
-        VcIconoEstado,
-        TruckIcon
+        VcDetalleEmpresa,
+        VcFormEmpresa
     },
-    mixins: [
-        ObtenerInstanciaMixin,
-        DarBajaInstanciaMixin,
-        DarAltaInstanciaMixin
-    ],
     data() {
         return {
             id: null,
-            empresa: null
-        }
-    },
-    computed: {
-        titulo() {
-            return `Perfil de ${this.empresa.nombre_fantasia}`;
-        },
-        
-        esMayorista() {
-            return this.empresa.es_mayorista === 1 ? true : false;
-        },
-        
-        esProveedor() {
-            return ! _.isNull(this.empresa.proveedor);
-        },
-        
-        conJuridicas() {
-            return ! _.isEmpty(this.empresa.juridicas);
-        },
-                
-        eliminado() {
-            return ! _.isNull(this.empresa.deleted_at);
-        },
-        
-        urlEspecifica() {
-            return `${this.$options.static.url.empresas}/${this.id}`;
+            empresa: {
+                nombre_fantasia: '',
+                es_mayorista: null,
+                created_at: new Date(),
+                updated_at: new Date(),
+                deleted_at: new Date()
+            },
+            ui: {
+                detalle: {
+                    visible: true
+                }
+            }
         }
     },
     watch: {
@@ -146,9 +64,7 @@ export default {
             if (id !== null) this.obtener();
         }
     },
-    created() {
-        this.resetearEmpresa();
-        
+    created() {        
         BusEventos.$on('VcTablaEmpresas:verPerfil', (id) => { this.setID(id) });
         BusEventos.$on('VcTablaEmpresas:restaurada', (id) => { this.actualizar(id) });
         BusEventos.$on('VcFormEmpresa:guardada', (id) => { this.actualizar(id) });
@@ -157,14 +73,6 @@ export default {
         url: {
             empresas: '/empresas'
         },
-        
-        empresaPorDefecto: {
-            nombre_fantasia: '',
-            es_mayorista: null,
-            created_at: new Date(),
-            updated_at: new Date(),
-            deleted_at: new Date()
-        }
     },
     methods: {
         setID(id) {
@@ -179,71 +87,34 @@ export default {
         
         obtener() {
             this.$_obtenerInstanciaMixin_obtener(
-                this.urlEspecifica,
+                `${this.$options.static.url.empresas}/${this.id}`,
                 (response) => { this.empresa = response.data }
             );
         },
         
-        editar() {
+        editarEmpresa() {
+            this.ui.detalle.visible = false;
             BusEventos.$emit('VcPerfilEmpresa:editar', this.id);
-            this.$emit('mostrar-form');
         },
         
-        altaProveedor() {
-            axios.post(`${this.urlEspecifica}/proveedor`)
-            .then((response) => {
-                this.obtener();
-                BusEventos.$emit('VcPerfilEmpresa:altaProveedor');
-                
-                this.exito({
-                    title: 'Éxito',
-                    message: response.data.mensaje
-                });
-            })
-            .catch((error) => {
-                this.error({
-                    title: 'Error',
-                    message: response.data.mensaje
-                });
-            })
-        },
-        
-        bajaProveedor() {
-            this.$_darBajaInstanciaMixin_eliminar(
-                `${this.urlEspecifica}/proveedor`,
-                () => {
-                    this.obtener();
-                    BusEventos.$emit('VcPerfilEmpresa:bajaProveedor');
-                }
-            );
-        },
-        
-        darDeBaja() {
-            this.$_darBajaInstanciaMixin_eliminar(
-                this.urlEspecifica,
-                () => {
-                    this.obtener();
-                    BusEventos.$emit('VcPerfilEmpresa:eliminada')
-                }
-            );
-        },
-        
-        darDeAlta() {
-            this.$_darAltaInstanciaMixin_restaurar(
-                `${this.urlEspecifica}/restore`,
-                () => {
-                    this.obtener();
-                    BusEventos.$emit('VcPerfilEmpresa:restaurada')
-                }
-            )
+        verEmpresa() {
+            this.ui.detalle.visible = true;
         },
         
         cerrar() {
             this.$emit('cerrar');
+        }
+    },
+    notifications: {
+        exito: {
+            title: 'Exito',
+            message: '',
+            type: 'success'
         },
-        
-        resetearEmpresa() {
-            this.empresa = { ...this.$options.static.empresaPorDefecto };
+        error: {
+            title: 'Error',
+            message: '',
+            type: 'error'
         }
     }
 }
