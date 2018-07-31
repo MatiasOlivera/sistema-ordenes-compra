050a01dfc8221bc79e16e1485aea783c903d3614
Matías Olivera <matiasolivera@outlook.com>
2018-07-30 23:47:58 -0300

    Agregar el componente BaseFormSimple

---
+++ resources/assets/js/components/BaseFormSimple.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+    
+    <form @submit.prevent="guardar">        
+        <!-- Campos -->
+        <slot></slot>
+        
+        <!-- Botones -->
+        <div class="row mt-4">
+            <div class="col">
+                <button type="submit" name="submit" class="btn btn-primary btn-sm">
+                    <save-icon class="icono"></save-icon> Guardar
+                </button>
+            </div>
+        </div>
+    </form>
+    
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import { SaveIcon } from 'vue-feather-icons';
+
+
+export default {
+    name: 'base-form-simple',
+    components: {
+        SaveIcon
+    },
+    methods: {
+        guardar() {
+            this.$emit('guardar');
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
