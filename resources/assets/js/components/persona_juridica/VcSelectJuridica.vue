ab51e612dfde02ea35d2894144a1310fe85f8119
Matías Olivera <matiasolivera@outlook.com>
2018-07-30 23:32:36 -0300

    Recibir las jurídicas en una prop en el componente VcSelectJuridica
    Ya no se permite la selección múltiple para no tener que calcular cuales son las nuevas relaciones y qué relaciones han sido eliminadas. Ver el código eliminado en el commit 6832b670cb580c377d31420b059e250a9908d96d

---
+++ resources/assets/js/components/persona_juridica/VcSelectJuridica.vue
@@ -0,0 +1,85 @@
+<template lang="html">
+    
+    <multiselect
+        v-model="seleccionada"
+        :id="$options.name"
+        :options="juridicas"
+        :multiple="$options.static.multiple"
+        :track-by="$options.static.trackBy"
+        :custom-label="juridicaConCuit"
+        :searchable="searchable"
+        :placeholder="placeholder"
+        :internal-search="internalSearch"
+        :select-label="selectLabel"
+        :select-group-label="selectGroupLabel"
+        :selected-label="selectedLabel"
+        :deselect-label="deselectLabel"
+        :deselect-group-label="deselectGroupLabel"
+        :limit-text="limitText"
+        :loading="estaCargando"
+        :open-direction="openDirection"
+        @search-change="buscarJuridicas"
+        @input="input"
+    >
+        <template slot="maxElements">
+            Haz llegado al máximo permitido de opciones seleccionadas. Primero quite una opción seleccionada para elegir otra.
+        </template>
+        
+        <template slot="noResult">
+            No se encontraron registros que coincidan con tu búsqueda.
+        </template>
+    </multiselect>
+    
+</template>
+
+<script>
+/**
+ * Mixins
+ */
+import MultiselectConfigMixin from '../../mixins/multiselect_config_mixin.js';
+import FiltroCuitMixin        from '../../mixins/persona_juridica/filtro_cuit_mixin.js';
+
+export default {
+    name: 'vc-select-juridica',
+    mixins: [
+        MultiselectConfigMixin,
+        FiltroCuitMixin
+    ],
+    props: {
+        juridicas: {
+            type: Array,
+            required: true
+        },
+        estaCargando: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            seleccionada: []
+        }
+    },
+    static: {
+        multiple: false,
+        trackBy: 'id'
+    },
+    methods: {
+        buscarJuridicas(valorBuscado) {
+            this.$emit('buscar', valorBuscado);
+        },
+        
+        juridicaConCuit({ denominacion, cuit }) {
+            let cuitConFormato = this.$options.filters.formatoCuit(cuit);
+            return `${denominacion} (${cuitConFormato})`;
+        },
+        
+        input(seleccionada) {
+            this.$emit('input', seleccionada);
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
