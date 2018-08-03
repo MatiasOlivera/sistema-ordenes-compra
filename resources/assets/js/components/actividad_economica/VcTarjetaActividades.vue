ef72eaa4f4b1b9172fed9da00f89ebf6dc17b193
Matías Olivera <matiasolivera@outlook.com>
2018-08-03 11:58:00 -0300

    Agregar la tarjeta que permite ver el listado de actividades asociadas

---
+++ resources/assets/js/components/actividad_economica/VcTarjetaActividades.vue
@@ -0,0 +1,124 @@
+<template lang="html">
+
+    <base-tarjeta
+        :titulo="$options.static.tarjeta.titulo"
+        :botonNuevoVisible="!form.visible"
+        :botonIzqVisible="form.visible"
+        @nuevo="mostrarForm"
+        @cerrar="ocultarForm"
+    >
+        
+        <template v-if="form.visible">
+            <vc-form-asociar-actividad
+                :actividades="actividadesNoAsociadas"
+                @buscar="buscar"
+                @guardar="darDeAlta"
+            >
+            </vc-form-asociar-actividad>
+            <hr>
+        </template>
+        
+        <template v-if="hayActividades">
+            <div class="row">
+                <div class="col-10">
+                    <h6>Descripción</h6>
+                </div>
+                <div class="col-2">
+                </div>
+            </div>
+
+            <vc-tarjeta-actividades-item
+                v-for="actividad in actividades"
+                :key="actividad.id"
+                :id="actividad.id"
+                :actividad="actividad.descripcion"
+                @dar-de-alta="darDeAlta"
+                @dar-de-baja="darDeBaja"
+            >
+            </vc-tarjeta-actividades-item>
+        </template>
+
+        <template v-else>
+            <p>{{ mensaje }}</p>
+        </template>
+
+    </base-tarjeta>
+
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import BaseTarjeta              from '../../components/BaseTarjeta.vue';
+import VcTarjetaActividadesItem from './VcTarjetaActividadesItem.vue';
+import VcFormAsociarActividad   from './VcFormAsociarActividad.vue';
+
+
+export default {
+    name: 'vc-tarjeta-actividades',
+    components: {
+        BaseTarjeta,
+        VcTarjetaActividadesItem,
+        VcFormAsociarActividad
+    },
+    props: {
+        actividadesNoAsociadas: {
+            type: Array,
+            default: function() {
+                return [];
+            }
+        },
+        actividades: {
+            type: Array,
+            required: true
+        },
+        mensaje: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            form: {
+                visible: false
+            }
+        }
+    },
+    computed: {
+        hayActividades() {
+            return !_.isEmpty(this.actividades);
+        }
+    },
+    static: {
+        tarjeta: {
+            titulo: 'Rubros'
+        }
+    },
+    methods: {
+        buscar(valorBuscado) {
+            this.$emit('buscar', valorBuscado);
+        },
+
+        darDeAlta({ id }) {
+            this.ocultarForm();
+            this.$emit('dar-de-alta', id);
+        },
+
+        darDeBaja(id) {
+            this.$emit('dar-de-baja', id);
+        },
+
+        mostrarForm() {
+            this.form.visible = true;
+        },
+
+        ocultarForm() {
+            this.form.visible = false;
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/actividad_economica/VcTarjetaActividadesItem.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+
+    <div class="row">
+        <div class="col-10">
+            <p>{{ actividad }}</p>
+        </div>
+        <div class="col-2">
+            <vc-boton-baja
+                :tooltip="$options.static.tooltip"
+                :nombre="actividad"
+                @confirmado="darDeBaja"
+            >
+            </vc-boton-baja>
+        </div>
+    </div>
+
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import VcBotonBaja from '../../components/VcBotonBaja.vue';
+
+
+export default {
+    name: 'vc-tarjeta-actividades-item',
+    components: { VcBotonBaja },
+    props: {
+        id: {
+            type: Number,
+            required: true
+        },
+        actividad: {
+            type: String,
+            required: true
+        }
+    },
+    static: {
+        tooltip: 'Eliminar relación'
+    },
+    methods: {
+        darDeBaja() {
+            this.$emit('dar-de-baja', this.id);
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
