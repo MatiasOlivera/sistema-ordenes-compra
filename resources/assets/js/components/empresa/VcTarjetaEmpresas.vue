4d47649a93fc06e92e8ab8f8d1aa5af861517109
Matías Olivera <matiasolivera@outlook.com>
2018-08-04 20:40:56 -0300

    Agregar la tarjeta que permite ver el listado de empresas asociadas

---
+++ resources/assets/js/components/empresa/VcTarjetaEmpresas.vue
@@ -0,0 +1,120 @@
+<template lang="html">
+
+    <base-tarjeta
+        :titulo="titulo"
+        :botonNuevoVisible="!form.visible"
+        :botonIzqVisible="form.visible"
+        @nuevo="mostrarForm"
+        @cerrar="ocultarForm"
+    >
+        
+        <template v-if="form.visible">
+            <vc-form-asociar-empresa
+                :empresas="empresasNoAsociadas"
+                @buscar="buscar"
+                @guardar="darDeAlta"
+            >
+            </vc-form-asociar-empresa>
+            <hr>
+        </template>
+        
+        <template v-if="hayEmpresas">
+            <div class="row">
+                <div class="col-10">
+                    <h6>Nombre de fantasía</h6>
+                </div>
+                <div class="col-2">
+                </div>
+            </div>
+
+            <vc-tarjeta-empresas-item
+                v-for="empresa in empresas"
+                :key="empresa.id"
+                :id="empresa.id"
+                :nombre="empresa.nombre_fantasia"
+                @dar-de-alta="darDeAlta"
+                @dar-de-baja="darDeBaja"
+            >
+            </vc-tarjeta-empresas-item>
+        </template>
+
+        <template v-else>
+            <p>{{ mensaje }}</p>
+        </template>
+
+    </base-tarjeta>
+
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import BaseTarjeta           from '../../components/BaseTarjeta.vue';
+import VcTarjetaEmpresasItem from './VcTarjetaEmpresasItem.vue';
+import VcFormAsociarEmpresa  from './VcFormAsociarEmpresa.vue';
+
+
+export default {
+    name: 'vc-tarjeta-empresas',
+    components: {
+        BaseTarjeta,
+        VcTarjetaEmpresasItem,
+        VcFormAsociarEmpresa
+    },
+    props: {
+        empresasNoAsociadas: {
+            type: Array,
+            default: function() {
+                return [];
+            }
+        },
+        empresas: {
+            type: Array,
+            required: true
+        },
+        mensaje: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            titulo: 'Empresas',
+            form: {
+                visible: false
+            }
+        }
+    },
+    computed: {
+        hayEmpresas() {
+            return ! _.isEmpty(this.empresas);
+        }
+    },
+    methods: {
+        buscar(valorBuscado) {
+            this.$emit('buscar', valorBuscado);
+        },
+        
+        darDeAlta({ id }) {
+            this.ocultarForm();
+            this.$emit('dar-de-alta', id);
+        },
+        
+        darDeBaja(id) {
+            this.$emit('dar-de-baja', id);
+        },
+        
+        mostrarForm() {
+            this.form.visible = true;
+        },
+        
+        ocultarForm() {
+            this.form.visible = false;
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/empresa/VcTarjetaEmpresasItem.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+
+    <div class="row">
+        <div class="col-10">
+            <p>{{ nombre }}</p>
+        </div>
+        <div class="col-2">
+            <vc-boton-baja
+                :tooltip="tooltip"
+                :nombre="nombre"
+                @confirmado="darDeBaja"
+            >
+            </vc-boton-baja>
+        </div>
+    </div>
+
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import VcBotonBaja from '../../components/VcBotonBaja.vue';
+
+
+export default {
+    name: 'vc-tarjeta-empresas-item',
+    components: { VcBotonBaja },
+    props: {
+        id: {
+            type: Number,
+            required: true
+        },
+        nombre: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            tooltip: 'Eliminar relación'
+        }
+    },
+    methods: {
+        darDeBaja() {
+            this.$emit('dar-de-baja', this.id);
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
