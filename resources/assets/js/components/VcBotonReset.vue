29e7bca472546e1fc16199bcf2531c7edd65e791
Matías Olivera <matiasolivera@outlook.com>
2018-08-17 19:37:57 -0300

    Agregar el componente VcBotonReset

---
+++ resources/assets/js/components/VcBotonReset.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    
+    <button
+        type="reset"
+        name="reset"
+        class="btn btn-secondary btn-sm"
+    >
+        <delete-icon class="icono"></delete-icon> Deshacer todo
+    </button>
+    
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import { DeleteIcon } from 'vue-feather-icons';
+
+export default {
+    name: 'vc-boton-reset',
+    components: { DeleteIcon }
+}
+</script>
+
+<style lang="css">
+</style>
