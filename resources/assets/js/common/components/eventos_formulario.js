636be617659d06a94e62c4c042d7b5bdad32c1dc
Matías Olivera <matiasolivera@outlook.com>
2018-08-17 19:42:15 -0300

    Agregar los eventos que puede emitir un formulario

---
+++ resources/assets/js/common/components/eventos_formulario.js
@@ -0,0 +1,23 @@
+/**
+ * Se quiere guardar los datos de la instancia
+ * Payload: la instancia actual
+ */
+export const EVENTO_GUARDAR = 'guardar';
+
+/**
+ * Se quiere restaurar los datos del formulario
+ * Payload: no tiene
+ */
+export const EVENTO_RESTAURAR = 'restaurar';
+
+/**
+ * La instancia ha sido guardada correctamente
+ * Payload: id de la instancia
+ */
+export const EVENTO_GUARDADO = 'guardado';
+
+/**
+ * La instancia ha sido actualizada correctamente
+ * Payload: no tiene
+ */
+export const EVENTO_ACTUALIZADO = 'actualizado';
