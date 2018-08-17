4b95ba815567c0cac51a90def047d9f18f72ea5d
Matías Olivera <matiasolivera@outlook.com>
2018-08-17 19:37:48 -0300

    Agregar el componente VcBotonSubmit

---
+++ resources/assets/js/components/VcBotonSubmit.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+
+    <button
+        type="submit"
+        name="submit"
+        class="btn btn-primary btn-sm"
+    >
+        <save-icon class="icono"></save-icon> Guardar
+    </button>
+
+</template>
+
+<script>
+/**
+ * Componentes
+ */
+import { SaveIcon } from 'vue-feather-icons';
+
+
+export default {
+    name: 'vc-boton-submit',
+    components: { SaveIcon }
+}
+</script>
+
+<style lang="css">
+</style>
