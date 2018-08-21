c02cf7cee6a27650ccd717e7ecddb3085ab18f17
Matías Olivera <matiasolivera@outlook.com>
2018-08-20 21:57:53 -0300

    Agregar el componente que muestra el detalle de un tipo de org.

---
+++ resources/assets/js/components/tipo_organizacion/VcDetalleTipoOrg.vue
@@ -0,0 +1,135 @@
+<template lang="html">
+    
+    <base-perfil
+        :titulo="titulo"
+        :nombreInstancia="tipoOrganizacion.descripcion"
+        :eliminado="eliminado"
+        @editar="editar"
+        @dar-de-baja="darDeBaja"
+        @dar-de-alta="darDeAlta"
+        @cerrar="cerrar"
+    >
+        <dl>
+            <dt>Descripcion:</dt>
+            <dd>{{ tipoOrganizacion.descripcion }}</dd>
+            
+            <dt>Creado:</dt>
+            <dd>
+                {{ tipoOrganizacion.created_at | moment('from') }}, 
+                {{ tipoOrganizacion.created_at | moment('L LT a') }}
+            </dd>
+            
+            <dt>Actualizado:</dt>
+            <dd>
+                {{ tipoOrganizacion.updated_at | moment('from') }}, 
+                {{ tipoOrganizacion.updated_at | moment('L LT a') }}
+            </dd>
+            
+            <template v-if="eliminado">
+                <dt>Eliminado:</dt>
+                <dd>
+                    {{ tipoOrganizacion.deleted_at | moment('from') }}, 
+                    {{ tipoOrganizacion.deleted_at | moment('L LT a') }}
+                </dd>
+            </template>
+        </dl>
+    </base-perfil>
+    
+</template>
+
+<script>
+import { apiTipoOrganizacion }    from '../../common/api/tipo_organizacion.js';
+import { objetoTienePropiedades } from '../../common/components/validadores.js';
+import { TIPO_CLAVES }            from '../../common/components/tipo_organizacion.js';
+
+/**
+ * Componentes
+ */
+import BasePerfil from '../../components/BasePerfil.vue';
+
+
+export default {
+    name: 'vc-detalle-tipo-org',
+    components: { BasePerfil },
+    props: {
+        tipoOrganizacion: {
+            type: Object,
+            validator: function(tipo) {
+                return objetoTienePropiedades(tipo, TIPO_CLAVES);
+            },
+            required: true
+        }
+    },
+    computed: {
+        titulo() {
+            return `Detalle de ${this.tipoOrganizacion.descripcion}`;
+        },
+        
+        eliminado() {
+            return _.isNull(this.tipoOrganizacion.deleted_at) ? false : true;
+        }
+    },
+    methods: {
+        editar() {
+            this.$emit('editar');
+        },
+        
+        cerrar() {
+            this.$emit('cerrar');
+        },
+        
+        darDeBaja() {
+            apiTipoOrganizacion.darDeBaja(this.tipoOrganizacion.id)
+            .then((respuesta) => {
+                const STATUS = respuesta.status;
+
+                if (STATUS === 200) {
+                    this.exito(respuesta.notificacion);
+                    
+                    this.$emit('dado-de-baja');
+                    BusEventos.$emit('VcPerfilTipoOrg:eliminado')
+                }
+            })
+            .catch(({notificacion}) => {
+                if (notificacion) {
+                    this.error(notificacion);
+                }
+            });
+        },
+        
+        darDeAlta() {
+            apiTipoOrganizacion.darDeAlta(this.tipoOrganizacion.id)
+            .then((respuesta) => {
+                const STATUS = respuesta.status;
+
+                if (STATUS === 200) {
+                    this.exito(respuesta.notificacion);
+                    
+                    this.$emit('dado-de-alta');
+                    BusEventos.$emit('VcPerfilTipoOrg:restaurado')
+                }
+            })
+            .catch(({notificacion}) => {
+                if (notificacion) {
+                    this.error(notificacion);
+                }
+            });
+        }
+    },
+    notifications: {
+        exito: {
+            title: 'Exito',
+            message: '',
+            type: 'success'
+        },
+        error: {
+            title: 'Error',
+            message: '',
+            type: 'error'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
