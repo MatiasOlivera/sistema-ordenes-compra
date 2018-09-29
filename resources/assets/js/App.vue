10b2135015ab7999e4c17dd5920443711d2dc768
Matías Olivera <matiasolivera@outlook.com>
2018-09-29 20:10:43 -0300

    Crear el componente App

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <la-cabecera/>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import LaCabecera from './components/LaCabecera.vue';
+
+export default {
+  name: 'App',
+  components: { LaCabecera }
+};
+</script>
+
+<style>
+</style>
