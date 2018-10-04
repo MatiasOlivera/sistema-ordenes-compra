02caca09bbf5110370ac97529b3a3f12bd76388b
Matías Olivera <matiasolivera@outlook.com>
2018-10-04 16:16:57 -0300

    Agregar los componentes titulo y botón nuevo
    Posibilita importarlos cuando se los necesite y se deja de depender del componente base crud

---
+++ resources/assets/js/components/ElTitulo.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="text-center mb-2">
+    <h1 class="h3">{{ titulo }}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ElTitulo',
+
+  props: {
+    titulo: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ resources/assets/js/components/VcBotonNuevo.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <button
+    type="button"
+    name="nuevo"
+    class="btn btn-primary"
+  >
+    <plus-circle-icon class="icono"/> {{ descripcion }}
+  </button>
+
+</template>
+
+<script>
+import { PlusCircleIcon } from 'vue-feather-icons';
+
+export default {
+  name: 'VcBotonNuevo',
+
+  components: { PlusCircleIcon },
+
+  props: {
+    descripcion: {
+      type: String,
+      required: true,
+      validator: function(descripcion) {
+        const BOTONES = ['Nuevo', 'Nueva'];
+        return BOTONES.includes(descripcion);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
