6762b358949d7ffac9fd821d373bc67f68b785b0
Matías Olivera <matiasolivera@outlook.com>
2018-10-23 20:27:19 -0300

    Agregar source map cuando se compila para producción

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,13 @@ let mix = require('laravel-mix');
  |
  */
 
-mix
-  .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css');
+const notInProduction = !mix.inProduction();
 
-mix.webpackConfig({
-  devtool: 'source-map'
-});
+mix.js('resources/assets/js/app.js', 'public/js');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+if (notInProduction) {
+  mix.webpackConfig({
+    devtool: 'source-map'
+  });
+}
